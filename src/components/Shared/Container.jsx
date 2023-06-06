322e62446ecb3f20a0c6837ff2efcf099af7a248
Mukhles <mukhlesurrhaman43@gmail.com>
2023-06-07 01:53:20 +0600

    client site all ready to project

---
+++ src/components/Shared/Container.jsx
@@ -0,0 +1,5 @@
+const Container = ({ children }) => {
+  return <div className="max-w-[2050px] mx-auto md:px-5 px-3">{children}</div>;
+};
+
+export default Container;
---
+++ src/components/Shared/NavBar/Navbar.jsx
@@ -0,0 +1,25 @@
+import Container from "../Container";
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div className="py-4 border-[1px]">
+        <Container>
+          <div className="flex justify-between items-center">
+            <div>
+                logo
+            </div>
+            <div>
+                menu
+            </div>
+            <div>
+                userProfile
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
